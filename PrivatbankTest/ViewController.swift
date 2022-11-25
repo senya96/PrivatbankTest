9b6b0d1ca1e9e6a92c3183e6a06d1b3b809e59d9
Sergiy <sergiynasinnuk@gmail.com>
2022-11-25 12:41:41 +0200

    Initial Commit

---
+++ PrivatbankTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrivatbankTest
+//
+//  Created by Sergiy Nasinnyk on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
